738bee637eeafe9e063f33b17b10bb4e8e842667
thuc nhi <thuc-nhi.wiedenhofer@laolateforme.io>
2022-08-05 09:25:15 +0200

    install context and firebase

---
+++ firebase/config.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyDPi4B6o-cXhcnQZCXKlBvPNMuLAzuZWKY',
+  authDomain: 'hrnet-f27e6.firebaseapp.com',
+  projectId: 'hrnet-f27e6',
+  storageBucket: 'hrnet-f27e6.appspot.com',
+  messagingSenderId: '952948591967',
+  appId: '1:952948591967:web:3bb90a7b369a7c75623aea',
+};
+
+export default firebaseConfig;
